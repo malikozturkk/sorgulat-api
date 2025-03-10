3870d6b4dc57f105c7aae81741d96069c855c078
malikozturkk <malikozturk975@gmail.com>
2025-03-10 17:00:46 +0300

    feat: add middleware in main file

--- main.go
+++ main.go
@@ -10,13 +10,29 @@ import (
 	"sorgulat-api/timezones/search"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	http.HandleFunc("/timezones/city", city.GetCityTimeZone)
-	http.HandleFunc("/timezones/country", country.GetCountryTimeZone)
-	http.HandleFunc("/timezones/", timezones.GetTimezoneBySlug)
-	http.HandleFunc("/timezones/difference/", difference.GetDifferenceBySlug)
-	http.HandleFunc("/timezones/search", search.SearchHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/timezones/city", city.GetCityTimeZone)
+	mux.HandleFunc("/timezones/country", country.GetCountryTimeZone)
+	mux.HandleFunc("/timezones/", timezones.GetTimezoneBySlug)
+	mux.HandleFunc("/timezones/difference/", difference.GetDifferenceBySlug)
+	mux.HandleFunc("/timezones/search", search.SearchHandler)
 
 	log.Println("Server is running on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", corsMiddleware(mux)))
 }
