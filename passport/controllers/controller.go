96b8792b9039771e49c7ad99f57fc1056245f7ab
Malik <malikozturk975@gmail.com>
2025-03-28 16:10:23 +0300

    feat: add blog url data in countries and passport controller

--- passport/controllers/controller.go
+++ passport/controllers/controller.go
@@ -11,15 +11,23 @@ func GetCountriesPassport(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	counts := make(map[string]int)
+	var updatedCountries []models.PassportCountries
+
 	for _, item := range passport.Countries {
 		counts[item.Value]++
+
+		updatedCountries = append(updatedCountries, models.PassportCountries{
+			Country: item.Country,
+			Value:   item.Value,
+			BlogUrl: item.BlogUrl,
+		})
 	}
 
 	response := struct {
 		Countries []models.PassportCountries `json:"countries"`
 		Counts    map[string]int             `json:"counts"`
 	}{
-		Countries: passport.Countries,
+		Countries: updatedCountries,
 		Counts:    counts,
 	}
 
--- passport/models/models.go
+++ passport/models/models.go
@@ -3,4 +3,5 @@ package models
 type PassportCountries struct {
 	Country string `json:"country"`
 	Value   string `json:"value"`
+	BlogUrl string `json:"blogUrl,omitempty"`
 }
